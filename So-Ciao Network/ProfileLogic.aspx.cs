564155c12e722aea5407af081da58509cb4456ec
Steve Waltin <stevewaltinjoy@gmail.com>
2020-09-07 22:24:41 +0530

    Initiated the pages for profile view user

---
+++ So-Ciao Network/ProfileLogic.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace So_Ciao_Network
+{
+    public partial class ProfileLogic : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
